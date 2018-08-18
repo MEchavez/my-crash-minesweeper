33fb6fdb2ad7d2bcde0bf67dee882d47a708acd7
Jose Ludian <ludiancohen@gmail.com>
2018-08-18 09:33:43 -0500

    Board generation complete

--- src/Board.java
+++ src/Board.java
@@ -3,12 +3,14 @@ import java.security.SecureRandom;
 public class Board {
 
     private int[][] grid;
+    private final int MINE = -1;
 
     public Board(int height, int width, int mines) {
-        insertMines(width, height, mines);
+        insertMines(height, width, mines);
+        insertAdjacentValues(height, width);
     }
 
-    private void insertMines(int width, int height, int mines) {
+    private void insertMines(int height, int width, int mines) {
         this.grid = new int[height][width];
         SecureRandom sr = new SecureRandom();
         int length = height * width;
@@ -33,6 +35,26 @@ public class Board {
             this.grid[row][col] = (index < mines) ? -1 : 0;
         }
         
+    }
+
+    private void insertAdjacentValues(int height, int width){
+        for(int row=0; row<height; row++) {
+            for(int col=0; col<width; col++){
+                for(int subrow = row-1; subrow < row+2; subrow++){
+                    for(int subcol = col-1; subcol < col+2; subcol++){
+                        if(isMine(row, col) && subrow>=0 && subrow<height && 
+                            subcol>=0 && subcol < width &&
+                            !(subrow == row && subcol == col) &&
+                            !isMine(subrow,subcol))
+                        this.grid[subrow][subcol]++;
+                    }
+                }                                
+            }
+        }
+    }
+
+    private boolean isMine(int row, int col) {
+        return this.grid[row][col] == this.MINE;
     }
 
 }
